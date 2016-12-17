a3d05d8098371928f3ce335d5ce6f3194cada52f
Jon Austen <djangofan@gmail.com>
2016-12-17 13:55:34 -0800

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,3 +4,12 @@ if Capybara.current_driver == :selenium
   headless = Headless.new
   headless.start
 end
+
+Before do
+  print '.'
+end
+
+at_exit do
+  print "\n"
+  print "\#\#\# All Cucumber tests finished. \#\#\#\n"
+end
